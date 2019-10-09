b649d8c3d932663dcbcfc9d0b347614c2cfa8cdc
Patrik Szewczyk <Patrik.Szewczyk@gmail.com>
2019-10-09 22:27:09 +0200

    theme added

---
+++ src/theme.ts
@@ -0,0 +1,15 @@
+import createTheme from 'spectacle/lib/themes/default';
+
+export const theme = createTheme(
+  {
+    primary: '#FFF',
+    secondary: '#1F2022',
+    tertiary: '#03A9FC',
+    quaternary: '#CECECE',
+    quinary: '#EE5080'
+  },
+  {
+    primary: 'Montserrat',
+    secondary: 'Helvetica'
+  }
+);
