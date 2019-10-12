ec7ce58c885d1652fd6fa03b0a937971025a302b
Patrik Szewczyk <Patrik.Szewczyk@gmail.com>
2019-10-12 23:32:37 +0200

    Slides: Outro added

--- src/slides/Outro/Outro.tsx
+++ src/slides/Outro/Outro.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import { Heading, Slide, SlideProps } from 'spectacle';
 
-export const AboutMe: React.FC<SlideProps> = (props: SlideProps) => (
+export const Outro: React.FC<SlideProps> = (props: SlideProps) => (
   <Slide bgColor="secondary" {...props}>
-    <Heading size={1} textColor="primary" fit caps>
-      {`About me`}
+    <Heading textColor="primary" textAlign={`right`}>
+      {`Thank you.`}
     </Heading>
   </Slide>
 );
