288f307841e33453a832bbc9a99496be50993741
Patrik Szewczyk <Patrik.Szewczyk@gmail.com>
2019-10-12 23:54:31 +0200

    Updates

---
+++ precache-manifest.07fe782893d6ca582c9d98538362a4b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f36814f86cf0aa2d2a0d",
+    "url": "/from-zero-to-hero/static/js/main.f36814f8.chunk.js"
+  },
+  {
+    "revision": "d1ef187620268eab79e8",
+    "url": "/from-zero-to-hero/static/css/1.17579cad.chunk.css"
+  },
+  {
+    "revision": "d1ef187620268eab79e8",
+    "url": "/from-zero-to-hero/static/js/1.d1ef1876.chunk.js"
+  },
+  {
+    "revision": "03277ec83cd9cbbdb2d5",
+    "url": "/from-zero-to-hero/static/js/runtime~main.03277ec8.js"
+  },
+  {
+    "revision": "c80ba71d9a29e2ec9d86062acefe0fce",
+    "url": "/from-zero-to-hero/index.html"
+  }
+];
