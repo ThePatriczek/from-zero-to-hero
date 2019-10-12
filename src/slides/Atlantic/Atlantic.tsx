ad010554d028e41e1af952b9494e13e8011350cd
Patrik Szewczyk <Patrik.Szewczyk@gmail.com>
2019-10-12 23:36:24 +0200

    Slides: Library added

--- src/slides/Atlantic/Atlantic.tsx
+++ src/slides/Atlantic/Atlantic.tsx
@@ -1,10 +1,24 @@
 import * as React from 'react';
-import { Heading, Slide, SlideProps } from 'spectacle';
+import { Heading, Slide, SlideProps, Text } from 'spectacle';
+import { quinary, tertiary, theme } from '../../theme';
 
 export const Atlantic: React.FC<SlideProps> = (props: SlideProps) => (
   <Slide bgColor="secondary" {...props}>
-    <Heading size={1} textColor="primary" fit caps>
-      {`Atlantic`}
+    <Heading textColor="primary" fit>
+      {`Custom component library`}
     </Heading>
+
+    <Text textColor={`primary`}>
+      <span>{`React Atlantic `}</span>
+      <span>{`(`}</span>
+      <span style={{ color: tertiary }}>{`Atla`}</span>
+      <span>{`s `}</span>
+      <span style={{ color: quinary }}>{`c`}</span>
+      <span>{`o`}</span>
+      <span style={{ color: tertiary }}>{`n`}</span>
+      <span>{`sul`}</span>
+      <span style={{ color: tertiary }}>{`ti`}</span>
+      <span>{`ng)`}</span>
+    </Text>
   </Slide>
 );
