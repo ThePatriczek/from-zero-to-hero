4733dbd9e7942d405d0a43293e4909f6149b6c8c
Patrik Szewczyk <Patrik.Szewczyk@gmail.com>
2019-10-12 23:30:28 +0200

    Theme constants

--- src/theme.ts
+++ src/theme.ts
@@ -1,12 +1,18 @@
 import createTheme from 'spectacle/lib/themes/default';
 
+export const primary = '#FFF';
+export const secondary = '#1F2022';
+export const tertiary = '#03A9FC';
+export const quaternary = '#CECECE';
+export const quinary = 'hotpink';
+
 export const theme = createTheme(
   {
-    primary: '#FFF',
-    secondary: '#1F2022',
-    tertiary: '#03A9FC',
-    quaternary: '#CECECE',
-    quinary: '#EE5080'
+    primary,
+    secondary,
+    tertiary,
+    quaternary,
+    quinary
   },
   {
     primary: 'Montserrat',
