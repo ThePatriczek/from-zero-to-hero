56f08ad3b7a31f8d0841c6b7680f0bf5fc4046d4
Patrik Szewczyk <Patrik.Szewczyk@gmail.com>
2019-10-16 20:49:47 +0200

    Slides: topics refactored

--- src/slides/Topics/Topics.tsx
+++ src/slides/Topics/Topics.tsx
@@ -1,10 +1,25 @@
 import * as React from 'react';
-import { Heading, Slide, SlideProps, List, ListItem } from 'spectacle';
+import { Heading, Slide, SlideProps, List, ListItem, Cite } from 'spectacle';
 
 export const Topics: React.FC<SlideProps> = (props: SlideProps) => (
   <Slide bgColor="secondary" {...props}>
-    <Heading size={1} textColor="tertiary" fit>
-      {`Topics`}
-    </Heading>
+    <Heading size={1} textColor="tertiary">{`Topics`}</Heading>
+    <List>
+      <ListItem>
+        <Cite textColor={`primary`}>{`GraphQL`}</Cite>
+      </ListItem>
+      <ListItem>
+        <Cite textColor={`primary`}>{`React Apollo`}</Cite>
+      </ListItem>
+      <ListItem>
+        <Cite textColor={`primary`}>{`GraphQL Code Generator`}</Cite>
+      </ListItem>
+      <ListItem>
+        <Cite textColor={`primary`}>{`React Hooks`}</Cite>
+      </ListItem>
+      <ListItem>
+        <Cite textColor={`primary`}>{`Custom component library`}</Cite>
+      </ListItem>
+    </List>
   </Slide>
 );
