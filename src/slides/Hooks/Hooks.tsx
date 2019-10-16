180494429ee91ca325376ee68acc8353f4ba64e8
Patrik Szewczyk <Patrik.Szewczyk@gmail.com>
2019-10-16 20:49:34 +0200

    Slides: hooks refactored

--- src/slides/Hooks/Hooks.tsx
+++ src/slides/Hooks/Hooks.tsx
@@ -1,5 +1,13 @@
 import * as React from 'react';
-import { Heading, Slide, SlideProps } from 'spectacle';
+import {
+  Heading,
+  Slide,
+  SlideProps,
+  List,
+  ListItem,
+  Text,
+  Cite
+} from 'spectacle';
 import { Example } from './Example';
 
 export const Hooks: React.FC<SlideProps> & { Example: React.FC<SlideProps> } = (
@@ -9,6 +17,20 @@ export const Hooks: React.FC<SlideProps> & { Example: React.FC<SlideProps> } = (
     <Heading textColor="quinary" fit>
       {`React Hooks`}
     </Heading>
+    <List>
+      <ListItem>
+        <Text
+          textColor={`primary`}
+        >{`Reausable methods for React Functional Components`}</Text>
+      </ListItem>
+      <ListItem>
+        <Cite textColor={`primary`}>
+          {`Be `}
+          <strong>{`modern`}</strong>
+          {` and clear, simplify your code!`}
+        </Cite>
+      </ListItem>
+    </List>
   </Slide>
 );
 
