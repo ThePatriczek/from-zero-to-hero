ab78201e4b444e1dfcce99c0854d2422d2095080
Patrik Szewczyk <Patrik.Szewczyk@gmail.com>
2019-10-16 20:49:20 +0200

    Slides: React atlantic edited

--- src/slides/Atlantic/Atlantic.tsx
+++ src/slides/Atlantic/Atlantic.tsx
@@ -1,24 +1,72 @@
 import * as React from 'react';
-import { Heading, Slide, SlideProps, Text } from 'spectacle';
-import { quinary, tertiary, theme } from '../../theme';
+import { Slide, SlideProps } from 'spectacle';
+import {
+  Button,
+  Checkbox,
+  Input,
+  Switch,
+  theme,
+  ThemeProvider,
+  Typography
+} from 'react-atlantic';
+import { Radio } from 'react-atlantic/lib/components/Radio';
+
+const { Title, Text, Paragraph, Link } = Typography;
 
 export const Atlantic: React.FC<SlideProps> = (props: SlideProps) => (
-  <Slide bgColor="secondary" {...props}>
-    <Heading textColor="primary" fit>
-      {`Custom component library`}
-    </Heading>
+  <Slide
+    bgColor="secondary"
+    textColor={`primary`}
+    {...props}
+    style={{ zoom: 1.5 }}
+  >
+    <ThemeProvider theme={theme}>
+      <Title level={1}>{`Our library`}</Title>
+      <Title level={2}>{`of reusable`}</Title>
+      <Title level={3}>{`custom components`}</Title>
+      <Paragraph>
+        <Text>{`CSS-in-JS (StyledComponents)`}</Text>
+      </Paragraph>
+      <Paragraph>
+        <Text>{`Type definitions`}</Text>
+      </Paragraph>
+      <Paragraph>
+        <Text>{`Unit tests`}</Text>
+      </Paragraph>
+      <Paragraph>
+        <Text isCopyable>{`Documentation`}</Text>
+      </Paragraph>
+      <Paragraph>
+        <Text isEditable>{`Plyaground`}</Text>
+      </Paragraph>
+      <Paragraph>
+        <Link
+          href={`https://thepatriczek.github.io/React-Atlantic/`}
+          target={`_blank`}
+        >{`Storybook`}</Link>
+      </Paragraph>
 
-    <Text textColor={`primary`}>
-      <span>{`React Atlantic `}</span>
-      <span>{`(`}</span>
-      <span style={{ color: tertiary }}>{`Atla`}</span>
-      <span>{`s `}</span>
-      <span style={{ color: quinary }}>{`c`}</span>
-      <span>{`o`}</span>
-      <span style={{ color: tertiary }}>{`n`}</span>
-      <span>{`sul`}</span>
-      <span style={{ color: tertiary }}>{`ti`}</span>
-      <span>{`ng)`}</span>
-    </Text>
+      <Input placeholder={`Some text...`} />
+      <Button type={'primary'}>{`Click`}</Button>
+
+      <div>
+        <Checkbox isDefaultChecked>
+          <Text>{`Checkbox`}</Text>
+        </Checkbox>
+      </div>
+      <div>
+        <Switch isDefaultChecked />
+      </div>
+      <div>
+        <Radio.Group onChange={() => null}>
+          <Radio value={0}>
+            <Text>{`Radio 1`}</Text>
+          </Radio>
+          <Radio value={1} isDefaultChecked>
+            <Text>{`Radio 2`}</Text>
+          </Radio>
+        </Radio.Group>
+      </div>
+    </ThemeProvider>
   </Slide>
 );
