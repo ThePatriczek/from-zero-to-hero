95510e07f5f79eefd777675404c0354118a4f394
Patrik Szewczyk <Patrik.Szewczyk@gmail.com>
2019-10-16 20:56:52 +0200

    Presentation link added

--- src/slides/Outro/Outro.tsx
+++ src/slides/Outro/Outro.tsx
@@ -1,10 +1,19 @@
 import * as React from 'react';
-import { Heading, Slide, SlideProps } from 'spectacle';
+import { Heading, Slide, SlideProps, List, ListItem } from 'spectacle';
+import { Link } from 'react-atlantic/lib/components/Typography/Link';
 
 export const Outro: React.FC<SlideProps> = (props: SlideProps) => (
   <Slide bgColor="secondary" {...props}>
     <Heading textColor="primary" textAlign={`right`}>
       {`Thank you.`}
     </Heading>
+    <List>
+      <ListItem textAlign={`right`}>
+        <Link
+          href={`https://thepatriczek.github.io/from-zero-to-hero/`}
+          target={`_blank`}
+        >{`https://thepatriczek.github.io/from-zero-to-hero/`}</Link>
+      </ListItem>
+    </List>
   </Slide>
 );
